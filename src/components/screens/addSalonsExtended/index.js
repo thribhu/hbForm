4eeeae2588f9977ce1507f1e137416e1245cbbf6
thribhuvan <thribhu.kumar@gmail.com>
2019-01-27 21:23:55 +0530

    few changes with picker

--- src/components/screens/addSalonsExtended/index.js
+++ src/components/screens/addSalonsExtended/index.js
@@ -124,87 +124,89 @@ class AddSalonExtended extends Component {
             >
             {
               formikProps => (
+                <View>
                 <CardItem>
-                                        <View style={{flexDirection: 'row'}}>
-                                             <View style={{flex:1}}>
-                                                <Picker
-                                                placeholder = "Type"
-                                                placeholderStyle={{color: 'black'}}
-                                                mode="dropdown"
-                                                style={{ width: 120 }}
-                                                value= {formikProps.values.businessType}
-                                                
-                                                onValueChange={formikProps.handleChange('businessType')}
-                                                >
-                                                {
-                                                    _.map(this.props.category, (val) => {
-                                                        return (
-                                                        <Picker.Item label={val.Category} value={val.CategoryId} />
-                                                        )
-                                                    })
-                                                }
-                                                </Picker>
-                                            </View> 
-                                            <View style={{flex:1}}>
-                                                <Picker
-                                                placeholder = "Class"
-                                                placeholderStyle={{color: 'black'}}
-                                                mode="dropdown"
-                                                style={{ width: 120 }}
-                                                selectedValue={formikProps.values.classes}
-                                                onValueChange={formikProps.handleChange('classes')}
-                                                >
-                                                {
-                                                    _.map(this.props.classes, (val) => {
-                                                        return (
-                                                        <Picker.Item label={val.ClassName} value={val.ClassId} />
-                                                        )
-                                                    })
-                                                }
-                                                </Picker>
-                                            </View> 
-                                        </View>
-                                        </CardItem>
-                                        <CardItem>
-                                                <View style={{flexDirection: 'row'}}>
-                                                        <View style={{flex:1}}>
-                                                        <Picker
-                                                        placeholder = "City"
-                                                        placeholderStyle={{color: 'black'}}
-                                                        mode="dropdown"
-                                                        style={{ width: 120 }}
-                                                        selectedValue={formikProps.values.city}
-                                                        onValueChange={formikProps.handleChange('city')}
-                                                        >
-                                                        {
-                                                            _.map(this.props.cities, (val) => {
-                                                                return (
-                                                                <Picker.Item label={val.CityName} value={val.CityId} />
-                                                                )
-                                                            })
-                                                        }
-                                                        </Picker>
-                                                    </View> 
-                                                    <View style={{flex:1}}>
-                                                        <Picker
-                                                        placeholder = "Area"
-                                                        placeholderStyle={{color: 'black'}}
-                                                        mode="dropdown"
-                                                        style={{ width: 120 }}
-                                                        selectedValue={formikProps.values.area}
-                                                        onValueChange={formikProps.handleChange('area')}
-                                                        >
-                                                        {
-                                                            _.map(this.props.operatingAreas, (val) => {
-                                                                return (
-                                                                <Picker.Item label={val.AreaName} value={val.AreaId} />
-                                                                )
-                                                            })
-                                                        }
-                                                        </Picker>
-                                                    </View> 
+            <View style={{flexDirection: 'row'}}>
+                  <View style={{flex:1}}>
+                    <Picker
+                    placeholder = "Type"
+                    placeholderStyle={{color: 'black'}}
+                    mode="dropdown"
+                    style={{ width: 120 }}
+                    value= {formikProps.values.businessType}
+                    
+                    onValueChange={formikProps.handleChange('businessType')}
+                    >
+                    {
+                        _.map(this.props.category, (val) => {
+                            return (
+                            <Picker.Item label={val.Category} value={val.CategoryId} />
+                            )
+                        })
+                    }
+                    </Picker>
+                </View> 
+                <View style={{flex:1}}>
+                    <Picker
+                    placeholder = "Class"
+                    placeholderStyle={{color: 'black'}}
+                    mode="dropdown"
+                    style={{ width: 120 }}
+                    selectedValue={formikProps.values.classes}
+                    onValueChange={formikProps.handleChange('classes')}
+                    >
+                    {
+                        _.map(this.props.classes, (val) => {
+                            return (
+                            <Picker.Item label={val.ClassName} value={val.ClassId} />
+                            )
+                        })
+                    }
+                    </Picker>
+                </View> 
+            </View>
+            </CardItem>
+            <CardItem>
+                    <View style={{flexDirection: 'row'}}>
+                            <View style={{flex:1}}>
+                            <Picker
+                            placeholder = "City"
+                            placeholderStyle={{color: 'black'}}
+                            mode="dropdown"
+                            style={{ width: 120 }}
+                            selectedValue={formikProps.values.city}
+                            onValueChange={formikProps.handleChange('city')}
+                            >
+                            {
+                                _.map(this.props.cities, (val) => {
+                                    return (
+                                    <Picker.Item label={val.CityName} value={val.CityId} />
+                                    )
+                                })
+                            }
+                            </Picker>
+                        </View> 
+                        <View style={{flex:1}}>
+                            <Picker
+                            placeholder = "Area"
+                            placeholderStyle={{color: 'black'}}
+                            mode="dropdown"
+                            style={{ width: 120 }}
+                            selectedValue={formikProps.values.area}
+                            onValueChange={formikProps.handleChange('area')}
+                            >
+                            {
+                                _.map(this.props.operatingAreas, (val) => {
+                                    return (
+                                    <Picker.Item label={val.AreaName} value={val.AreaId} />
+                                    )
+                                })
+                            }
+                            </Picker>
+                        </View> 
                                                 </View>
                                         </CardItem>
+                                        </View>
               )
             }
             </Formik>
